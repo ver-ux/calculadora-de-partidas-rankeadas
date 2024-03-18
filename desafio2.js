a80197317c853e6f70a0eae67d3357094fd6c604
Vergilio Mendes <seu-email>
2024-03-18 20:21:11 -0300

    first commit

---
+++ desafio2.js
@@ -0,0 +1,29 @@
+function calcularNivel(vitorias, derrotas) {
+
+    let saldoVitorias = vitorias - derrotas;
+    
+    let nivel;
+    if (vitorias < 10) {
+        nivel = "Ferro";
+    } else if (vitorias >= 11 && vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias >= 21 && vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias >= 51 && vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias >= 81 && vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias >= 91 && vitorias <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
+
+    return { saldoVitorias, nivel };
+}
+
+
+let vitorias = 75;
+let derrotas = 25;
+let resultado = calcularNivel(vitorias, derrotas);
+console.log(`O Herói tem um saldo de ${resultado.saldoVitorias} e está no nível de ${resultado.nivel}`);
